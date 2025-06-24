e8cdd1a08a6641d729e907871908a86b686376d9
ArthurLiberato1 <arthurliberatoeug@hotmail.com>
2025-06-24 17:57:24 -0300

    first commit

---
+++ src/main/java/com/github/arthurliberato1/studycontrolbackend/StudyControlBackendApplication.java
@@ -0,0 +1,13 @@
+package com.github.arthurliberato1.studycontrolbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudyControlBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StudyControlBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/github/arthurliberato1/studycontrolbackend/StudyControlBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.arthurliberato1.studycontrolbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudyControlBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
